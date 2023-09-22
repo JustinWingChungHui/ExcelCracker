f6b13df47ae86aad767d3d04e00cb7a0724c7535
Justin Hui <j.hui@smartinmedia.com>
2023-09-22 22:40:01 +0100

    merge

---
+++ Generator.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace ExcelCrack
+{
+    public class Generator
+    {
+        static char[] characters = {
+                        '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j' ,'k','l','m','n','o','p',
+                        'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','C','L','M','N','O','P',
+                        'Q','R','S','T','U','V','X','Y','Z','!','?',' ','*','-','+'
+        };
+        private int[] _counter;
+        private int _length;
+        private object once = new object();
+
+        public Generator(int length)
+        {
+
+            _counter = new int[length];
+            _length = length;
+        }
+
+        public string generateNextPassword()
+        {
+            lock (once)
+            {
+                var result = new StringBuilder();
+                for (int i = 0; i < _length; i++)
+                {
+                    var counter = _counter[i];
+                    result.Append(characters[counter]);
+                }
+
+
+                IncrementCounter(_length - 1);
+
+
+                return result.ToString();
+            }
+        }
+
+        private void IncrementCounter(int index)
+        {
+
+            if (_counter[index] >= characters.Length - 1)
+            {
+                _counter[index] = 0;
+                if (index > 0)
+                {
+                    IncrementCounter(index - 1);
+                }
+                else
+                {
+                    _length++;
+                    _counter = new int[_length];
+                }
+            }
+            else
+            {
+                _counter[index]++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExcelCrack;
+using ExcelDataReader;
+using System.IO;
+using System.Timers;
+
+Console.WriteLine("Starting!");
+
+// https://github.com/ExcelDataReader/ExcelDataReader#important-note-on-net-core
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+//your password
+var filePath = args.Length > 0 ? args[0] : "C:\\Users\\smartinmedia\\Desktop\\protected workbook.xlsx";
+if (!File.Exists(filePath))
+{
+    Console.WriteLine($"File does not exist {filePath}");
+    return;
+}
+
+var generator = new Generator(1);
+var threads = 1;
+
+var attempts = 0;
+var timer = new System.Timers.Timer(1000);
+timer.Elapsed += OnTimedEvent;
+
+void OnTimedEvent(object? sender, ElapsedEventArgs e)
+{
+    var attemptsPersecond = attempts;
+    Console.WriteLine($"Attempts per second {attemptsPersecond}");
+    attempts = 0;
+}
+
+timer.Start();
+
+using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+{
+    var tasks = new List<Task>();
+    for (var thread = 1; thread <= threads; thread++)
+    {
+        MemoryStream ms = new MemoryStream();
+        await stream.CopyToAsync(ms);
+
+        tasks.Add(Task.Run(() => CrackStream(ms, generator)));
+
+    }  
+
+    await Task.WhenAny(tasks);
+}
+
+void CrackStream(MemoryStream ms, Generator generator)
+{
+    bool success = false;
+    do
+    {
+        var password = generator.generateNextPassword();
+        var config = new ExcelReaderConfiguration
+        {
+            Password = password
+        };
+
+        try
+        {
+            Console.WriteLine($"Trying {password}");
+            using (var reader = ExcelReaderFactory.CreateReader(ms, config))
+            {
+                reader.Read();
+            }
+
+
+            Console.WriteLine($"Success! Password is {password}");
+            success = true;
+            return;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        attempts++;
+        success = false;
+    } while (!success);    
+}
